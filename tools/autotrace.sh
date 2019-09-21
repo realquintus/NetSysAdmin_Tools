7352940df25cd86741ab66a9b814a3a308f67b6d
pablo <pablohousse@gmail.com>
2019-09-21 17:16:55 +0200

    Autotrace now print traceroute, replace * by # and add \n at very end of line

--- tools/autotrace.sh
+++ tools/autotrace.sh
@@ -14,4 +14,7 @@ if ! [[ $(ping $1 -c1 | grep "%" | cut -d "," -f3 | sed "s/ //g" | cut -c1) == "
 	echo -e "\n L'hôte $1 est injoignable";
 	exit 1;
 fi
-echo "C'est bon"
+dst=$1;
+echo -e "L'hôte $dst est joignable lancement de traceroute...\n";
+trace_init=$(traceroute $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
+echo -e $trace_init;
