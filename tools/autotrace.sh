8303d35dc24687f80b39087052a78bc898fdb6ce
pablo <pablohousse@gmail.com>
2019-10-08 13:10:42 +0200

    Improving autotrace.sh

--- tools/autotrace.sh
+++ tools/autotrace.sh
@@ -17,7 +17,7 @@ fi
 dst=$1;
 echo -e "L'hôte $dst est joignable lancement de traceroute...\n";
 	# On lance un traceroute initial afin de déterminer les routeurs qui répondent pas, on remplace les * par des # et on ajoute \n a la fin de chaque pour que echo afin correctement la réponse
-trace_init=$(traceroute $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
+trace_init=$(traceroute -n $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
 	# On test si tout les routeurs ont répondus, dans ce cas on affiche la réponse et on arrète le script
 if [[ $(echo -e $trace_init | grep "# # #") == "" ]];then
 	echo -e "Tout les routeurs ont répondus du premier coup... Trop facile :\n\n";
@@ -26,33 +26,33 @@ if [[ $(echo -e $trace_init | grep "# # #") == "" ]];then
 fi
 	# On récupère le ttl des routeurs qui ne répondent pas
 compteur=1;
-while [ true  ];do
+
+while [ true  ];do #Boucle principale
 	echo "Testing default conf";
-	rep=$(traceroute -f $compteur -m $compteur $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
+	rep=$(traceroute -n -f $compteur -m $compteur $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
 	if [[ $(echo $rep | grep "# # #") == "" ]];then
 		echo -e $rep | grep -v "traceroute";
 	# Test si le routeur $compteur réponds a une trame tarceroute par défaut
 	else
 		echo "Testing ICMP";
-		rep=$(traceroute -f $compteur -m $compteur -I $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
+		rep=$(traceroute -n -f $compteur -m $compteur -I $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
 		if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
 			echo -e $rep | grep -v "traceroute";
 		# Test si le routeur répond à une trame ICMP
 		else
 			echo "Testing TCP";
-			rep=$(traceroute -f $compteur -m $compteur -T -p 25 $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
+			rep=$(traceroute -n -f $compteur -m $compteur -T -p 25 $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
 			if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
 				echo -e $rep | grep -v "traceroute"
 			else
-				echo "Router $compteur is unreachable";
+				echo -e "Router $compteur is unreachable\n";
 			fi;
 		fi;
 	fi;
 	compteur=$(($compteur + 1));
-	if ! [[ $(echo $rep | grep "$src") == "" ]];then
+	if ! [[ $(echo $rep | grep -v "hops" | grep $1) == "" ]];then
+		echo "$rep";
 		break;
-	fi;
-done;
-#ports qui marchent: 
-
-
+		fi;
+	echo $rep | grep "$1" | grep -v "traceroute";
+	done;
