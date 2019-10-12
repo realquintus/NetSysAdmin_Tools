75051df49c9333b20be0ab47aef2c6965886ba57
pablo <pablohousse@gmail.com>
2019-10-12 20:58:50 +0200

    Improving autotraceV2.sh to make it use options

--- tools/autotrace/autotraceV2.sh
+++ tools/autotrace/autotraceV2.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 usage (){
-	echo -e "Autotrace est un script automatisant l'utilisation de traceroute en essayant d'obtenir une réponse de tout les routeurs en faisant varier le port de destination ou le protocole. Entrez l'adresse IP destination en premier argument"
+	echo -e "Autorace is a bash script that will try to get a answer from every router in traceroute to the host. To do that, it will try many protocols and ports.\nOptions:\n\t-a : This option is required, enter the host IPv4 address after.\n\t-v : Verbosee option\n\t-h : Show this help message."
 }
+
+while getopts "hva:" option;do
+	echo $option $OPTARG;
+	case $option in
+		h)
+			usage
+			;;
+		v)
+			verb="true";
+			;;
+		a)
+			dst=$OPTARG;
+			;;
+	esac;
+done
+
 	# On test si l'argument 1 existe
-dst=${1:?$(usage)};	
+#dst=${1:?$(usage)};
 # On test si l'hôte est joignable
 if ! [[ $(ping $dst -c1 | grep "%" | cut -d "," -f3 | sed "s/ //g" | cut -c1) == "0" ]]; then
 	usage;
@@ -22,8 +38,6 @@ echo -e "L'hôte $dst est joignable lancement de traceroute...\n";
 #	echo -e $trace_init;
 #	exit 0;
 #fi
-
-echo -e "digraph NetMap {\n" >> NetMap.xdot;
 compteur=1;
 methods=("" " -I" " -T -p 25" " -T -p 123" " -T -p 22" " -T -p 80" " -T -p 443" " -U -p 21" " -U -p 53" " -U -p 68" " -U -p 69" " -U -p 179");
 
@@ -32,15 +46,17 @@ for compteur in $(seq 1 30);do #Boucle principale
 	for method in "${methods[@]}";do 	
 		rep=$(traceroute -n $method -f $compteur -m $compteur $dst | sed 's/*/#/g' | sed -n "2p");
 		if ! [[ $(echo $rep | grep "# # #") ]];then
-			echo -e "$rep\n" | awk '{print $2}' >> routes.txt;
+			rep=$(echo "$rep" | awk '{print $2}');
 			break;
 		elif [[ $method = " -U -p 179"  ]];then
-			echo "#" >> routes.txt;
+			rep="#";
 		fi;
 	done;
 	compteur=$(($compteur + 1));
 	if [[ -n $(echo -e $rep | grep $dst) ]];then
+		./mkgraph.sh "NetMap_to_$dst.dot" $rep 1
 		break;
-	fi;
-	
+	else
+		./mkgraph.sh "NetMap_to_$dst.dot" $rep;
+	fi;	
 	done;
---
+++ tools/autotrace/mkgraph.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=${1:?Please enter a file as first argument};
+host=${2:?Please enter a IPv4 address as second argument};
+end=${3:-0};
+
+if ! [[ -a ./$file ]];then
+	echo "digraph NetMap{" > $file;
+	echo -e "\tlocalhost -> " >> $file;
+fi;
+
+sed -i 's/ $/'" \"$host\";"'/' $file;
+
+if [ $end -eq 0 ];then
+	echo -e "\t\"$host\" -> " >> $file;
+else
+	echo "}" >> $file;
+fi;
+
