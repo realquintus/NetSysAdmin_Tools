64f8cf0d3f6d050a19e52a3fd59d9a4d6a087aa5
realquintus <pablohousse@gmail.com>
2020-10-02 17:44:42 +0200

    Improving gui_autotrace.sh

--- tools/autotrace/autotrace.sh
+++ tools/autotrace/autotrace.sh
@@ -1,95 +1,117 @@
 #!/bin/bash
 
+##################################################################################################
+#					AUTOTRACE SCRIPT					 #
+#     Web interface available at the following link: https://phousse.fr/projects/autotrace	 #
+##################################################################################################
 usage (){
-	echo -e "Autotrace est un script automatisant l'utilisation de traceroute en essayant d'obtenir une réponse de tout les routeurs en faisant varier le port de destination ou le protocole. Entrez l'adresse IP destination en premier argument"
+	echo -e "Autorace is a bash script that will try to get a answer from every router in traceroute to the host entered. To do that, it will try many protocols and ports. After receiving an answer from a router execute mkgraph.sh that add it to .dot file.\n\nOptions:\n\t-a : This option is required, enter the host's address after(FQDN or IPv4). You can also enter several hosts separated by ':'. In this case, the file will contain all the routes.\n\t-g : This option allows you to generate a graph\n\t\t-f : This option needs to be used with -g, otherwise it will be useless. -f is used to indicate the name of the graph file. Be careful if this file already exist, it will be erased. If this option is not entered, this file will be CURRENT_DIR/NetMap_HOST.dot\n\t-v : Verbose option\n\t-h : Show this help message.";
 }
-	# On test si l'argument 1 existe
-dst=${1:?usage};	
-# On test si l'hôte est joignable
-if ! [[ $(ping $dst -c1 | grep "%" | cut -d "," -f3 | sed "s/ //g" | cut -c1) == "0" ]]; then
+## loop to identify options
+while getopts "hva:f:g" option;do
+	case $option in
+		h)
+			usage
+			exit 0;
+			;;
+		v)
+			verb="true";
+			;;
+		a)
+			if ! [[ $(echo $OPTARG | grep ":") == "" ]];then
+				multiple_hosts="true";
+				nbr_host=$(($(echo $OPTARG | grep -o ":" | wc -l)+1));
+				list_host=$OPTARG				
+			else
+				nbr_host=1;
+				dst=$OPTARG;
+			fi;
+			;;
+		f)
+			file=$OPTARG;
+			;;
+		g)
+			graph="true"
+			;;
+	esac;
+done
+##
+if [ -z $nbr_host ];then
+	echo "Error: No host entered... Diplay usage message:"
 	usage;
-	echo -e "\n L'hôte $dst est injoignable";
 	exit 1;
 fi
-echo -e "L'hôte $dst est joignable lancement de traceroute...\n";
+if [ -z $file ];then # Set default value to $file
+	file="NetMap.dot"
+fi;
+if [[ $graph = "true" ]];then
+	echo "" > $file;	
+fi;
+methods=("" " -I" " -T -p 25" " -T -p 123" " -T -p 22" " -T -p 80" " -T -p 443" " -U -p 21" " -U -p 53" " -U -p 68"); #This array contain all options and arguments that will be used with traceroute
 
-	# On lance un traceroute initial afin de déterminer les routeurs qui répondent pas, on remplace les * par des # et on ajoute \n a la fin de chaque pour que echo afin correctement la réponse
-trace_init=$(traceroute -n $dst | sed 's/*/#/g' | sed 's/$/\\n/g');
-	# On test si tout les routeurs ont répondus, dans ce cas on affiche la réponse et on arrète le script
-
-if [[ $(echo -e $trace_init | grep "# # #") == "" ]];then
-	echo -e "Tout les routeurs ont répondus du premier coup... Trop facile :\n\n";
-	echo -e $trace_init;
-	exit 0;
-fi
-
-compteur=1;
-
-while [ true  ];do #Boucle principale
-	echo "Testing default configuration";
-	rep=$(traceroute -n -f $compteur -m $compteur $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-	if [[ $(echo $rep | grep "# # #") == "" ]];then
-		echo -e $rep | grep -v "traceroute";
-	# Test si le routeur $compteur réponds a une trame tarceroute par défaut
-	else
-		echo -e "\tICMP";
-		rep=$(traceroute -n -f $compteur -m $compteur -I $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-		if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-			echo -e $rep | grep -v "traceroute";
-		# Test si le routeur répond à une trame ICMP
-		else
-			echo -e "\tTCP  \n\t\ton smtp port";
-			rep=$(traceroute -n -f $compteur -m $compteur -T -p 25 $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-			if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-				echo -e $rep | grep -v "traceroute";
-			else
-				echo -e "\t\ton NTP port";
-				rep=$(traceroute -n -f $compteur -m $compteur -T -p 123 $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-				
-				if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-					echo -e $rep | grep -v "traceroute";
-				else
-					
-					echo -e "\t\ton SSH port";
-					rep=$(traceroute -n -f $compteur -m $compteur -T -p 22 $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-				
-					if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-						echo -e $rep | grep -v "traceroute";
-					else			
-			
-						echo -e "\t\ton HTTP port";
-						rep=$(traceroute -n -f $compteur -m $compteur -T -p 80 $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-						if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-							echo -e $rep | grep -v "traceroute";
-						else
-							
-							echo -e "\t\ton HTTPS port";
-							rep=$(traceroute -n -f $compteur -m $compteur -T -p 443 $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-							if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-								echo -e $rep | grep -v "traceroute";
-							else
-								echo -e "\tUDP";
-								declare -a ports=(21 53 68 69 179);
-								for i in ${ports[*]};do
-									rep=$(traceroute -n -f $compteur -m $compteur -p $i $dst | sed 's/*/#/g' | sed 's/$/\\n/g' | sed -n "2p");
-									echo -e "\t\t on port number $i";
-									if [[ $(echo -e  $rep | grep "# # #") == "" ]];then
-										echo -e $rep | grep -v "traceroute";
-										break;
-									fi;
-								done
-echo -e "Router $compteur is unreachable\n";
-							fi;
-						fi;
-					fi;
-				fi;
-			fi;
+### Loop for diferents hosts
+for i in $(seq 1 $nbr_host);do 
+	if [[ $multiple_hosts = "true" ]];then
+		dst=$(echo $list_host | cut -d":" -f1); # Extracting host from the list
+		list_host=$(echo $list_host | cut -d":" -f2-); # Remove the host that will be use from the list
+		if [[ $verb="true" ]];then
+			echo -e "\nHost n°$i:";
 		fi;
 	fi;
-	
-	compteur=$(($compteur + 1));
-	if [[ -n $(echo -e $rep | grep $dst) || $compteur == 30 ]];then
-		break;
+	compteur=1;
+	if ! [[ $(echo $dst | grep -E '[a-z]|[A-Z]') = "" ]];then
+		dst_fqdn=$dst;
+		dst=$(host $dst | sed -n '1p' | awk '{print $4}'); # DNS request
 	fi;
-	
+	#### Loop for number of hops
+	for compteur in $(seq 1 30);do
+		for method in "${methods[@]}";do 
+			if [[ $verb = "true" ]];then
+				echo -e "Trying: traceroute -q1 -n $method -f $compteur -m $compteur $dst"
+			fi;
+			rep=$(traceroute -A -z 500 -q1 -n $method -f $compteur -m $compteur $dst | sed 's/*/#/g' | sed -n "2p");# Traceroute command, replace * with # and print second line
+			if [[ $(echo $rep | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}') ]];then # Check if there is a IPv4 address in traceroute answer
+				if [[ $verb = "true" ]];then
+					echo -e "Answer received:\n\t$rep";
+				fi;
+				AS=$(echo $rep | awk '{print $3}' | sed 's/\[//' | sed 's/\]//'); # Store the AS number and delete "[" and "]" because it can it will interpreted by grep in mkgraph.sh
+				rep=$(echo "$rep" | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}'); # Store IPv4 address
+				break;
+			elif [[ $method = ${methods[-1]}  ]];then # Check if it is the last method available
+				rep="#$compteur";
+				AS="#";
+				if [[ $verb = "true" ]];then
+					echo "Routeur n°$compteur is unreachable";
+				fi;
+			fi;
+		done;
+		compteur=$(($compteur + 1));
+		if [[ $graph = "true" ]];then
+			if [[ $verb = "true" ]];then
+				echo -e "Adding $rep to the .dot file\n";
+			fi;
+			if [[ -n $(echo -e $rep | grep $dst) ]];then # Check if the IPv4 address is $dst
+				if ! [[ $dst_fqdn = "" ]];then
+					rep=$(echo "$rep:$dst_fqdn");
+				fi
+				if [ $i -eq $nbr_host ];then # Check if it's the last host to close the file
+					./mkgraph.sh -f $file -a $rep -A $AS -e file; # End of file
+				else
+					./mkgraph.sh -f $file -a $rep -A $AS -e route; # End of route
+				fi;
+			elif [ $compteur -eq 30 ];then
+				rep="Max hopes reached for host $dst"
+				./mkgraph.sh -f $file -a $rep -A $AS -e route
+			else
+				./mkgraph.sh -f $file -a $rep -A $AS;
+			fi;	
+		fi;
+		echo "$rep [$AS]";
+		if [[ -n $(echo -e $rep | grep $dst) ]];then # Check if the IPv4 address is $dst
+			break;
+		fi;
 	done;
+	####
+
+done;
+###
--- tools/autotrace/gui_autotrace.sh
+++ tools/autotrace/gui_autotrace.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
-echo "test"
-options=""
-dialog --backtitle "AutotraceV2 GUI" --title "Hosts" --inputbox "Enter the host's address (FQDN or IPv4). You can also enter several hosts separated by ':'" 10 60 2> /tmp/autotrace.tmp
-options="-a "$(cat /tmp/autotrace.tmp)
-dialog --backtitle "AutotraceV2 GUI" \
- 	--title "Generate a graph ?" \
-       --yesno "\nThis will create a .dot file that can be open with xdot\n\nDo you accept?" 10 30
-if [ $? -eq 0 ];then
-	options=$options" -g"
-	dialog --backtitle "AutotraceV2 GUI" --title "File name" --inputbox "Enter the name of the file with .dot at the end. If empty the file will be called NetMap.dot" 10 60 2> /tmp/autotrace.tmp
-	options=$options" -f $(cat /tmp/autotrace.tmp)"
-	file=$(cat /tmp/autotrace.tmp)
-	dialog --backtitle "AutotraceV2 GUI" \
-	 	--title "Generate a graph ?" \
-	       --yesno "\nDisplay the dot file after?\n" 10 30
+
+tool=$(dialog --backtitle "What do you want to do?" --title "Please choose your destiny" --menu "What tool do you want to use?" 20 61 5 "Autotrace" "A tool to automatize traceroute discovery by varying protocol and ports, it can also generate a beautily dot graph" "Mkgraph" "Another tool used by autotrace to create graphs, you can also use it to combine" --output-fd 1)
+if [[ $tool == "Autotrace" ]];then
+	cmd="./autotrace.sh -a "$(dialog --backtitle "AutotraceV2 GUI" --title "Hosts" --inputbox "Enter the host's address (FQDN or IPv4). You can also enter several hosts separated by ':'" 10 60 --output-fd 1)
+	dialog --backtitle "AutotraceV2 GUI" --title "Generate a graph ?" --yesno "\nThis will create a .dot file that can be open with xdot\n\nDo you accept?" 10 30
 	if [ $? -eq 0 ];then
-		disp="true"
+		cmd=$cmd" -g"
+		file=$(dialog --backtitle "AutotraceV2 GUI" --title "File name" --inputbox "Enter the name of the file with .dot at the end. If empty the file will be called NetMap.dot" 10 60 --output-fd 1)
+		if [[$file != "" ]];then
+			cmd=$cmd" -f $file"
+		fi
+		dialog --backtitle "AutotraceV2 GUI" --title "Generate a graph ?" --yesno "\nDisplay the dot file after?\n" 10 30 --output-fd 1
+		disp=$?
+		eval $cmd
+		if [ $disp -eq 0 ];then
+			xdot $file
+		fi
 	fi
+else
+	message=""
+	while (true);do
+		file1=$(dialog --colors --backtitle "Combine dot files" --title "File n°1" --inputbox "Enter the path to the first file\n\Z1 $message" 10 60 --output-fd 1)
+		if [[ -e $file1 ]];then
+			if [[ $file1 == $(echo $file1 | grep -E "*.dot") ]];then
+				cmd="./mkgraph.sh -f $file1"
+				break
+			else
+				message="The file you entered is not a *.dot file"
+			fi
+		else
+			message="The file you entered does not exist"
+		fi
+	done
+	message=""
+	while (true);do
+		file2=$(dialog --colors --backtitle "Combine dot files" --title "File n°2" --inputbox "Enter the path to the second file\n\Z1 $message" 10 60 --output-fd 1)
+		if [[ -e $file2 ]];then
+			if [[ $file2 == $(echo $file2 | grep -E "*.dot") ]];then
+				cmd=$cmd" -c $file2"
+				break
+			else
+				message="The file you entered is not a *.dot file"
+			fi
+		else
+			message="The file you entered does not exist"
+		fi
+	done
+	eval $cmd
 fi
-./autotraceV2.sh $options -v
-if [[ $disp == "true" ]];then
-	xdot $file
-fi
-rm /tmp/autotrace.tmp
