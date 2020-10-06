ebe1b9776508c5877f77ea79b028a0c695e1205f
realquintus <pablohousse@gmail.com>
2020-10-06 13:10:24 +0000

    Adding rights checks

--- tools/autotrace/gui_autotrace.sh
+++ tools/autotrace/gui_autotrace.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+touch /etc/test 2> /dev/null
+if [ $? -eq 0 ];then
+	rm /etc/test
+else
+	echo "Please run as root or sudo"
+	exit 2
+fi
 tool=$(dialog --backtitle "What do you want to do?" --title "Please choose your destiny" --menu "What tool do you want to use?" 20 61 5 "Autotrace" "A tool to automatize traceroute discovery by varying protocol and ports, it can also generate a beautily dot graph" "Mkgraph" "Another tool used by autotrace to create graphs, you can also use it to combine" --output-fd 1)
 if [[ $tool == "Autotrace" ]];then
+	sudo echo "" > /tmp/autotrace_output.tmp
 	host=$(dialog --backtitle "AutotraceV2 GUI" --title "Hosts" --inputbox "Enter the host's address (FQDN or IPv4). You can also enter several hosts separated by ':'" 10 60 --output-fd 1)
 	cmd="./autotrace.sh -a "$host
 	dialog --backtitle "AutotraceV2 GUI" --title "Generate a graph ?" --yesno "\nThis will create a .dot file that can be open with xdot\n\nDo you accept?" 10 30
@@ -14,7 +21,7 @@ if [[ $tool == "Autotrace" ]];then
 		dialog --backtitle "AutotraceV2 GUI" --title "Generate a graph ?" --yesno "\nDisplay the dot file after?\n" 10 30 --output-fd 1
 		disp=$?
 		nbr_host=$(($(echo $host | grep ":" | wc -l)+1))
-		eval $cmd >> /tmp/autotrace_output.tmp &
+		eval "sudo $cmd" >> /tmp/autotrace_output.tmp &
 		sleep 0.5
 		while (true)
 		do
@@ -28,7 +35,6 @@ if [[ $tool == "Autotrace" ]];then
 				progress=99
 			fi
 			if [[ $(tail -n 1 $file) == "}" ]];then
-				echo "test"
 				break
 			fi
 			dialog --title "Retrieving routers" --gauge "Answer received $(tail -n 1 /tmp/autotrace_output.tmp)" 10 70 $progress &
@@ -69,7 +75,5 @@ else
 			message="The file you entered does not exist"
 		fi
 	done
-	eval $cmd
+	eval "sudo $cmd"
 fi
-#rm /tmp/autotrace_gui.tmp 2> /dev/null
-#clear
